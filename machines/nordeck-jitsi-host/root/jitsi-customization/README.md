afdce2e0ba767374d6755c1695204f21b5deafad
emrah <emrah.eryilmaz@nordeck.net>
2022-10-27 13:27:57 +0300

    jitsi, feat: jitsi customization script

---
+++ machines/nordeck-jitsi-host/root/jitsi-customization/README.md
@@ -0,0 +1,21 @@
+## JITSI CUSTOMIZATION
+
+Add the customization notes, scripts and related files in this folder.
+
+#### TLS update
+
+Run the following command to regenerate the TLS certificates.
+
+```bash
+set-letsencrypt-cert ___JITSI_FQDN___,___TURN_FQDN___
+```
+
+#### Customization script
+
+Customize the `customize.sh` script according to your needs.
+
+Usage:
+
+```bash
+bash customize.sh
+```
---
+++ machines/nordeck-jitsi-host/root/jitsi-customization/customize.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# ------------------------------------------------------------------------------
+# This script customizes the Jitsi installation. Run it on the host machine.
+#
+# usage:
+#     bash customize.sh
+# ------------------------------------------------------------------------------
+BASEDIR=$(dirname $0)
+JITSI_ROOTFS="/var/lib/lxc/nordeck-jitsi/rootfs"
+JITSI_MEET_CONFIG="$JITSI_ROOTFS/etc/jitsi/meet/___JITSI_FQDN___-config.js"
+JITSI_MEET_INTERFACE="$JITSI_ROOTFS/usr/share/jitsi-meet/interface_config.js"
+JITSI_MEET_APP="$JITSI_ROOTFS/usr/share/jitsi-meet"
+
+# ------------------------------------------------------------------------------
+# config.js
+# ------------------------------------------------------------------------------
+[[ -f "$BASEDIR/files/___JITSI_FQDN___-config.js" ]] && \
+    cp $BASEDIR/files/___JITSI_FQDN___-config.js $JITSI_MEET_CONFIG
+
+# ------------------------------------------------------------------------------
+# interface_config.js
+# ------------------------------------------------------------------------------
+[[ -f "$BASEDIR/files/interface_config.js" ]] && \
+    cp $BASEDIR/files/interface_config.js $JITSI_MEET_INTERFACE
+
+# ------------------------------------------------------------------------------
+# custom files
+# ------------------------------------------------------------------------------
+[[ -f "$BASEDIR/files/favicon.ico" ]] && \
+    cp $BASEDIR/files/favicon.ico $JITSI_MEET_APP/
+[[ -f "$BASEDIR/files/favicon.ico" ]] && \
+    cp $BASEDIR/files/favicon.ico $JITSI_MEET_APP/images/
+[[ -f "$BASEDIR/files/watermark.svg" ]] && \
+    cp $BASEDIR/files/watermark.svg $JITSI_MEET_APP/images/
