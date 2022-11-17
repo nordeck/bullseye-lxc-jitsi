ada815a1695cff7f143daff3bff67550db6cd4e3
emrah <emrah.eryilmaz@nordeck.net>
2022-11-17 19:12:32 +0300

    jibr, feat: initialise sidecar installation script

---
+++ installer-sub-scripts/nordeck-jibri/302-component-sidecar.sh
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# COMPONENT-SIDECAR.SH
+# ------------------------------------------------------------------------------
+set -e
+source $INSTALLER/000-source
+
+# ------------------------------------------------------------------------------
+# ENVIRONMENT
+# ------------------------------------------------------------------------------
+MACH="nordeck-jibri-template"
+cd $MACHINES/$MACH
+
+ROOTFS="/var/lib/lxc/$MACH/rootfs"
+
+# ------------------------------------------------------------------------------
+# INIT
+# ------------------------------------------------------------------------------
+[[ "$DONT_RUN_COMPONENT_SIDECAR" = true ]] && exit
+
+echo
+echo "-------------------- COMPONENT SIDECAR --------------------"
+
+# ------------------------------------------------------------------------------
+# CONTAINER
+# ------------------------------------------------------------------------------
+# stop the ephemeral containers
+set +e
+systemctl stop jibri-ephemeral-container.service
+set -e
+
+# start the container
+lxc-start -n $MACH -d
+lxc-wait -n $MACH -s RUNNING
+
+# wait for the network to be up
+for i in $(seq 0 9); do
+    lxc-attach -n $MACH -- ping -c1 host.loc && break || true
+    sleep 1
+done
+
+# ------------------------------------------------------------------------------
+# PACKAGES
+# ------------------------------------------------------------------------------
+# fake install
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY -dy reinstall hostname
+EOS
+
+# update
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY update
+apt-get $APT_PROXY -y dist-upgrade
+EOS
+
+# packages
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY -y install gnupg
+apt-get $APT_PROXY -y install redis
+EOS
+
+# ------------------------------------------------------------------------------
+# CONTAINER SERVICES
+# ------------------------------------------------------------------------------
+lxc-attach -n $MACH -- systemctl stop jibri-xorg.service
+lxc-stop -n $MACH
+lxc-wait -n $MACH -s STOPPED
+
+# ------------------------------------------------------------------------------
+# CLEAN UP
+# ------------------------------------------------------------------------------
+find $ROOTFS/var/log/jitsi/jibri -type f -delete
+
+# ------------------------------------------------------------------------------
+# ON HOST
+# ------------------------------------------------------------------------------
+systemctl start jibri-ephemeral-container.service
