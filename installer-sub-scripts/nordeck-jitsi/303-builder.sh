3aead974e5ffadbba741becc6a4b4c495f60466c
emrah <emrah.eryilmaz@nordeck.net>
2022-11-17 11:25:02 +0300

    jitsi, feat: initialize builder script

---
+++ installer-sub-scripts/nordeck-jitsi/303-builder.sh
@@ -0,0 +1,104 @@
+# ------------------------------------------------------------------------------
+# BUILDER.SH
+# ------------------------------------------------------------------------------
+set -e
+source $INSTALLER/000-source
+
+# ------------------------------------------------------------------------------
+# ENVIRONMENT
+# ------------------------------------------------------------------------------
+MACH="nordeck-builder"
+cd $MACHINES/$MACH
+
+ROOTFS="/var/lib/lxc/$MACH/rootfs"
+
+# ------------------------------------------------------------------------------
+# INIT
+# ------------------------------------------------------------------------------
+[[ "$DONT_RUN_BUILDER" = true ]] && exit
+
+echo
+echo "-------------------------- $MACH --------------------------"
+
+# ------------------------------------------------------------------------------
+# CONTAINER SETUP
+# ------------------------------------------------------------------------------
+# stop the template container if it's running
+set +e
+lxc-stop -n nordeck-bullseye
+lxc-wait -n nordeck-bullseye -s STOPPED
+set -e
+
+# remove the old container if exists
+set +e
+lxc-stop -n $MACH
+lxc-wait -n $MACH -s STOPPED
+lxc-destroy -n $MACH
+rm -rf /var/lib/lxc/$MACH
+sleep 1
+set -e
+
+# create the new one
+lxc-copy -n nordeck-bullseye -N $MACH -p /var/lib/lxc/
+
+# the shared directories
+mkdir -p $SHARED/cache
+
+# the container config
+rm -rf $ROOTFS/var/cache/apt/archives
+mkdir -p $ROOTFS/var/cache/apt/archives
+
+cat >> /var/lib/lxc/$MACH/config <<EOF
+
+# Start options
+lxc.start.auto = 0
+lxc.start.order = 303
+lxc.start.delay = 2
+lxc.group = nordeck-group
+lxc.group = onboot
+EOF
+
+# start the container
+lxc-start -n $MACH -d
+lxc-wait -n $MACH -s RUNNING
+
+# wait for the network to be up
+for i in $(seq 0 9); do
+    lxc-attach -n $MACH -- ping -c1 host.loc && break || true
+    sleep 1
+done
+
+# ------------------------------------------------------------------------------
+# PACKAGES
+# ------------------------------------------------------------------------------
+# fake install
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY -dy reinstall hostname
+EOS
+
+# update
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY update
+apt-get $APT_PROXY -y dist-upgrade
+EOS
+
+# packages
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY -y install unzip git
+EOS
+
+# ------------------------------------------------------------------------------
+# SYSTEM CONFIGURATION
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# CONTAINER SERVICES
+# ------------------------------------------------------------------------------
+lxc-stop -n $MACH
+lxc-wait -n $MACH -s STOPPED
