b7cad924128842977894235465ddf388a37bcbb4
emrah <emrah.eryilmaz@nordeck.net>
2022-11-17 12:29:22 +0300

    jitsi, feat: pjproject build dependencies

--- installer-sub-scripts/nordeck-jitsi/304-builder-pjproject.sh
+++ installer-sub-scripts/nordeck-jitsi/304-builder-pjproject.sh
@@ -11,6 +11,8 @@ MACH="nordeck-builder"
 cd $MACHINES/$MACH
 
 ROOTFS="/var/lib/lxc/$MACH/rootfs"
+PJPROJECT_REPO="https://github.com/jitsi/pjproject"
+PJPROJECT_BRANCH="jibri-2.10-dev1"
 
 # ------------------------------------------------------------------------------
 # INIT
@@ -32,6 +34,19 @@ for i in $(seq 0 9); do
     lxc-attach -n $MACH -- ping -c1 host.loc && break || true
     sleep 1
 done
+
+# ------------------------------------------------------------------------------
+# PACKAGES
+# ------------------------------------------------------------------------------
+# pjproject releated packages
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY -y install libv4l-dev libsdl2-dev libavcodec-dev \
+    libavdevice-dev libavfilter-dev libavformat-dev libavresample-dev \
+    libavutil-dev libswresample-dev libswscale-dev libasound2-dev libopus-dev \
+    libvpx-dev
+EOS
 
 # ------------------------------------------------------------------------------
 # PJPROJECT
