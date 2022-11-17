5f1c586329c2d3bdfde219617afef01c9a932231
emrah <emrah.eryilmaz@nordeck.net>
2022-11-17 14:03:43 +0300

    jitsi, feat: building jitsi-component-selector

---
+++ installer-sub-scripts/nordeck-jitsi/305-builder-component-selector.sh
@@ -0,0 +1,74 @@
+# ------------------------------------------------------------------------------
+# COMPONENT-SELECTOR.SH
+# ------------------------------------------------------------------------------
+set -e
+source $INSTALLER/000-source
+
+# ------------------------------------------------------------------------------
+# ENVIRONMENT
+# ------------------------------------------------------------------------------
+MACH="nordeck-builder"
+cd $MACHINES/$MACH
+
+ROOTFS="/var/lib/lxc/$MACH/rootfs"
+PROJECT_REPO="https://github.com/jitsi/jitsi-component-selector.git"
+DEBFULLNAME="Nordeck Jitsi Team"
+DEBEMAIL="info@nordeck.net"
+
+# ------------------------------------------------------------------------------
+# INIT
+# ------------------------------------------------------------------------------
+[[ "$DONT_BUILD_COMPONENT_SELECTOR" = true ]] && exit
+
+echo
+echo "------------------- COMPONENT SELECTOR --------------------"
+
+# ------------------------------------------------------------------------------
+# CONTAINER
+# ------------------------------------------------------------------------------
+# start the container
+lxc-start -n $MACH -d
+lxc-wait -n $MACH -s RUNNING
+
+# wait for the network to be up
+for i in $(seq 0 9); do
+    lxc-attach -n $MACH -- ping -c1 host.loc && break || true
+    sleep 1
+done
+
+# ------------------------------------------------------------------------------
+# JITSI-COMPONENT-SELECTOR
+# ------------------------------------------------------------------------------
+# build
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+mkdir -p /home/dev/src
+chown dev:dev /home/dev/src
+rm -rf /home/dev/src/jitsi-component-selector*
+EOS
+
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+su -l dev <<EOSS
+    set -e
+
+    cd ~/src
+    git clone $PJPROJECT_REPO
+    cd ~/src/jitsi-component-selector/resources
+
+    export DEBFULLNAME=$DEBFULLNAME
+    export DEBEMAIL=$DEBEMAIL
+    bash build_deb_package.sh
+EOSS
+EOS
+
+# store
+mkdir -p /root/nordeck-store
+cp $ROOTFS/home/dev/src/jitsi-component-selector_*_all.deb \
+    /root/nordeck-store/jitsi-component-selector.deb
+
+# ------------------------------------------------------------------------------
+# CONTAINER SERVICES
+# ------------------------------------------------------------------------------
+lxc-stop -n $MACH
+lxc-wait -n $MACH -s STOPPED
