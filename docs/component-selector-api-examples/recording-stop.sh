d2816c23843991a99f10b50eafc52dc9424e95d6
emrah <emrah.eryilmaz@nordeck.net>
2022-11-28 12:35:11 +0300

    docs: add recording-stop example

--- docs/component-selector-api-examples/recording-stop.sh
+++ docs/component-selector-api-examples/recording-stop.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 set -e
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Packages:
 #   apt-get install curl jq
 #
 # Usage:
 #   export JITSI_HOST="https://jitsi.nordeck.corp"
 #   export JITSI_ROOM="myroom"
-#   export RECORDER_PASSWD="my-secret-password"
 #
-#   bash jibri-start.sh
+#   bash recording-stop.sh <SESSION_ID>
 # ------------------------------------------------------------------------------
+
+SESSION_ID="$1"
 
 JSON=$(cat <<EOF
 {
+  "sessionId": "$SESSION_ID",
   "callParams": {
     "callUrlInfo": {
       "baseUrl": "$JITSI_HOST",
       "callName": "$JITSI_ROOM"
     }
   },
-  "componentParams": {
-    "type": "JIBRI",
-    "region": "default-region",
-    "environment": "default-env"
-  },
   "metadata": {
     "sinkType": "FILE"
-  },
-  "callLoginParams": {
-    "domain": "recorder.jitsi.nordeck.corp",
-    "username": "recorder",
-    "password": "$RECORDER_PASSWD"
   }
 }
 EOF
 )
 
 curl -sk \
-  -X POST https://jitsi.nordeck.corp/jitsi-component-selector/sessions/start \
+  -X POST https://jitsi.nordeck.corp/jitsi-component-selector/sessions/stop \
   --header "Content-Type: application/json" \
   --data @- <<< $JSON | jq '.'
