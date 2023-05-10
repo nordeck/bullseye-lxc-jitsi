ca0e438dca47064750e86b6f6fac50948dbac9d0
emrah <emrah.eryilmaz@nordeck.net>
2023-05-10 17:52:26 +0300

    nordeck-sip-vm, feat: initialize installer for sip-vm

---
+++ installer-sub-scripts/nordeck-sip-vm/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# INIT.SH
+# ------------------------------------------------------------------------------
+set -e
+
+# ------------------------------------------------------------------------------
+# CHECKS
+# ------------------------------------------------------------------------------
+echo
+
+[[ -z "$JITSI_FQDN" ]] && echo "JITSI_FQDN not found" && false
+[[ -z "$(dig +short $JITSI_FQDN)" ]] && echo "unresolvable JITSI_FQDN" && false
+[[ -z "$JIBRI_PASSWD" ]] && echo "JIBRI_PASSWD not found" && false
+[[ -z "$JIBRI_SIP_PASSWD" ]] && echo "JIBRI_SIP_PASSWD not found" && false
