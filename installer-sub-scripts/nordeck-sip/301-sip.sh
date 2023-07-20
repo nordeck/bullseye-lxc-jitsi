8eb7e7bcd020675490d5de1e76908d704732e7e0
emrah <emrah.eryilmaz@nordeck.net>
2023-07-20 15:49:19 +0300

    nordeck-sip, fix: chromedriver download address

--- installer-sub-scripts/nordeck-sip/301-sip.sh
+++ installer-sub-scripts/nordeck-sip/301-sip.sh
@@ -147,12 +147,15 @@ EOS
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 CHROME_VER=\$(dpkg -s google-chrome-stable | egrep "^Version" | \
-    cut -d " " -f2 | cut -d. -f1)
-CHROMEDRIVER_VER=\$(curl -s \
-    https://chromedriver.storage.googleapis.com/LATEST_RELEASE_\$CHROME_VER)
-wget -T 30 -qO /tmp/chromedriver_linux64.zip \
-    https://chromedriver.storage.googleapis.com/\$CHROMEDRIVER_VER/chromedriver_linux64.zip
-unzip -o /tmp/chromedriver_linux64.zip -d /usr/local/bin/
+    cut -d " " -f2 | cut -d. -f1-3)
+CHROMELAB_LINK="https://googlechromelabs.github.io/chrome-for-testing"
+CHROMEDRIVER_LINK=\$(curl -s \
+    \$CHROMELAB_LINK/known-good-versions-with-downloads.json | \
+    jq -r ".versions[].downloads.chromedriver | select(. != null) | .[].url" | \
+    grep linux64 | grep "\$CHROME_VER" | tail -1)
+wget -T 30 -qO /tmp/chromedriver-linux64.zip \$CHROMEDRIVER_LINK
+unzip -o /tmp/chromedriver-linux64.zip -d /tmp
+mv /tmp/chromedriver-linux64/chromedriver /usr/local/bin/
 chmod 755 /usr/local/bin/chromedriver
 EOS
 
