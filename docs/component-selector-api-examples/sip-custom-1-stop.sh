20916e5160b67d673902c01297a46b2c04626e17
emrah <emrah.eryilmaz@nordeck.net>
2023-08-16 10:50:46 +0300

    docs: add the sample script to stop the custom SIP call

---
+++ docs/component-selector-api-examples/sip-custom-1-stop.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+set -e
+
+# -----------------------------------------------------------------------------
+# Packages:
+#   apt-get install curl
+#
+# Usage:
+#   export JITSI_HOST="https://jitsi.nordeck.corp"
+#   export JITSI_ROOM="myroom"
+#
+#   bash sip-custom-1-stop.sh <SESSION_ID>
+# ------------------------------------------------------------------------------
+
+SESSION_ID="$1"
+
+JSON=$(cat <<EOF
+{
+  "sessionId": "$SESSION_ID",
+  "callParams": {
+    "callUrlInfo": {
+      "baseUrl": "$JITSI_HOST",
+      "callName": "$JITSI_ROOM"
+    }
+  }
+}
+EOF
+)
+
+curl -sk \
+  -X POST https://jitsi.nordeck.corp/jitsi-component-selector/sessions/stop \
+  --header "Content-Type: application/json" \
+  --data @- <<< $JSON
