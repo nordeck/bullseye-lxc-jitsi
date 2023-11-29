1f59a6bd649cc71666df4e669d7c497936f947c8
emrah <emrah.eryilmaz@nordeck.net>
2023-11-29 11:19:45 +0300

    docs: add bearer token to the sip-inbound-start example

--- docs/component-selector-api-examples/sip-inbound-start.sh
+++ docs/component-selector-api-examples/sip-inbound-start.sh
@@ -14,12 +14,14 @@ set -e
 #   export INVITER_USERNAME="1009@sip.nordeck.corp"
 #   export INVITER_PASSWORD="1234"
 #   export INVITER_CONTACT="<sip:1009@172.17.17.36:5060;transport=udp>"
+#   export PRIVATE_KEY_FILE="./signal.key"
 #
 #   bash sip-inbound-start.sh <INVITEE>
 #
 # Example:
 #   bash sip-inbound-start.sh "sip:1001@sip.nordeck.corp"
 # ------------------------------------------------------------------------------
+[[ -z "$PRIVATE_KEY_FILE" ]] && PRIVATE_KEY_FILE="./signal.key" || true
 
 INVITEE="$1"
 DISPLAY_NAME=$(echo $INVITEE | cut -d: -f2 | cut -d@ -f1)
@@ -51,7 +53,18 @@ JSON=$(cat <<EOF
 EOF
 )
 
+# generate the bearer token
+HEADER=$(echo -n '{"alg":"RS256","typ":"JWT","kid":"jitsi/signal"}' | \
+  base64 | tr '+/' '-_' | tr -d '=\n')
+PAYLOAD=$(echo -n '{"iss":"signal","aud":"jitsi-component-selector"}' | \
+  base64 | tr '+/' '-_' | tr -d '=\n')
+SIGN=$(echo -n "$HEADER.$PAYLOAD" | \
+  openssl dgst -sha256 -binary -sign $PRIVATE_KEY_FILE | \
+  openssl enc -base64 | tr '+/' '-_' | tr -d '=\n')
+TOKEN="$HEADER.$PAYLOAD.$SIGN"
+
 curl -sk \
   -X POST $JITSI_HOST/jitsi-component-selector/sessions/start \
+  --header "Authorization: Bearer $TOKEN" \
   --header "Content-Type: application/json" \
   --data @- <<< $JSON | jq '.'
