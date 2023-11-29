ab059b014a78b0856f476e19fbb00ec865c85641
emrah <emrah.eryilmaz@nordeck.net>
2023-11-29 11:19:21 +0300

    docs: add bearer token to the sip-custom-start example

--- docs/component-selector-api-examples/sip-custom-1-start.sh
+++ docs/component-selector-api-examples/sip-custom-1-start.sh
@@ -11,12 +11,14 @@ set -e
 # Usage:
 #   export JITSI_HOST="https://jitsi.nordeck.corp"
 #   export JITSI_ROOM="myroom"
+#   export PRIVATE_KEY_FILE="./signal.key"
 #
 #   bash sip-custom-1-start.sh <DISPLAY_NAME>
 #
 # Example:
 #   bash sip-custom-1-start.sh "cisco"
 # ------------------------------------------------------------------------------
+[[ -z "$PRIVATE_KEY_FILE" ]] && PRIVATE_KEY_FILE="./signal.key" || true
 
 DISPLAY_NAME="$1"
 AUTO_ANSWER_TIMEOUT=360
@@ -46,7 +48,18 @@ JSON=$(cat <<EOF
 EOF
 )
 
+# generate the bearer token
+HEADER=$(echo -n '{"alg":"RS256","typ":"JWT","kid":"jitsi/signal"}' | \
+  base64 | tr '+/' '-_' | tr -d '=\n')
+PAYLOAD=$(echo -n '{"iss":"signal","aud":"jitsi-component-selector"}' | \
+  base64 | tr '+/' '-_' | tr -d '=\n')
+SIGN=$(echo -n "$HEADER.$PAYLOAD" | \
+  openssl dgst -sha256 -binary -sign $PRIVATE_KEY_FILE | \
+  openssl enc -base64 | tr '+/' '-_' | tr -d '=\n')
+TOKEN="$HEADER.$PAYLOAD.$SIGN"
+
 curl -sk \
   -X POST $JITSI_HOST/jitsi-component-selector/sessions/start \
+  --header "Authorization: Bearer $TOKEN" \
   --header "Content-Type: application/json" \
   --data @- <<< $JSON | jq '.'
