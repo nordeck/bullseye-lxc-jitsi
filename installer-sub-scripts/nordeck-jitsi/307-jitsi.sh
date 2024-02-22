7ca754e896b2ba80e0e9809fd2c8d8508cfaf57a
emrah <emrah.eryilmaz@nordeck.net>
2024-02-22 17:20:54 +0300

    nordeck-jitsi, fix: add missing comma

--- installer-sub-scripts/nordeck-jitsi/307-jitsi.sh
+++ installer-sub-scripts/nordeck-jitsi/307-jitsi.sh
@@ -400,7 +400,7 @@ sed -i "/muc_password_whitelist =/a \
 # muc_lobby_whitelist
 sed -i "/-- muc_lobby_whitelist/a \
 \    muc_lobby_whitelist = {\n\
-\        \"recorder@recorder.$JITSI_FQDN\"\n\
+\        \"recorder@recorder.$JITSI_FQDN\",\n\
 \        \"sip@sip.$JITSI_FQDN\"\n\
 \    }" \
     $ROOTFS/etc/prosody/conf.avail/$JITSI_FQDN.cfg.lua
