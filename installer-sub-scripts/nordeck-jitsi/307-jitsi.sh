8885cc2801b280bda03cb28f207366f527df61b3
emrah <emrah.eryilmaz@nordeck.net>
2024-02-22 16:46:49 +0300

    nordeck-jitsi, fix: dont put recorder into prosody admins

--- installer-sub-scripts/nordeck-jitsi/307-jitsi.sh
+++ installer-sub-scripts/nordeck-jitsi/307-jitsi.sh
@@ -380,9 +380,6 @@ sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" \
     $ROOTFS/etc/prosody/conf.avail/recorder.$JITSI_FQDN.cfg.lua
 ln -s ../conf.avail/recorder.$JITSI_FQDN.cfg.lua \
     $ROOTFS/etc/prosody/conf.d/
-sed -i "/Component.*conference.$JITSI_FQDN/,/admins/ \
-    s/admins = {/admins = { \"recorder@recorder.$JITSI_FQDN\",/" \
-    $ROOTFS/etc/prosody/conf.avail/$JITSI_FQDN.cfg.lua
 
 # sip
 cp etc/prosody/conf.avail/sip.cfg.lua \
